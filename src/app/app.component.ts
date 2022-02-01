9d83801ab849fa8ec66055466fb1dca72148ce2a
ManuelMosquera <mosqueramanuel5@gmail.com>
2022-01-31 22:37:30 -0500

    Simple Crud con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+
+  employeeArray: Employee[] = [
+    { id: 1, name: "Manuel", country: "Colombia" },
+    { id: 2, name: "Ryan", country: "Usa" },
+    { id: 3, name: "Lina", country: "Canadá" }
+  ]
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  delete() {
+    if (confirm('Are you sure you want to delete it?')) {
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+
+  }
 }
---
+++ src/app/models/employee.ts
@@ -0,0 +1,12 @@
+export class Employee {
+    public id: number;
+    public name: string;
+    public country: string;
+
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.country = "";
+
+    }
+}
